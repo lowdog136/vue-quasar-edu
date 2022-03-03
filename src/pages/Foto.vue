cd68fc284ffe4d94932b60a08daec724bcd35b7a
Denis Khodarev <lowdog136@gmail.com>
2022-03-03 09:43:14 +0200

    Добавление новых страниц

---
+++ src/pages/Foto.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout view="lHh lpr lFf" container style="height: 800px" class="shadow-2 rounded-borders">
+      <q-header elevated>
+        <q-toolbar>
+          <q-avatar>
+            <img  alt="logo" src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+
+          <q-toolbar-title>
+            О сайте
+          </q-toolbar-title>
+
+          <q-btn flat round dense icon="whatshot" />
+        </q-toolbar>
+      </q-header>
+
+      <q-page-container>
+        <q-page padding>
+          <p v-for="n in 1" :key="n">
+            Cтраница о истории сайта severfans.ru <br>
+            Сайт был основан в 2007 году.<br>
+            Точную дату не вспомнить, но судя по Wayback Machine - 25 мая. Наверно эта дата и будет считаться датой начала. Хотя по факту все начиналось раньше.<br>
+            Как клуб, объединение болельщиков (хулсы, ультрас) - этот сайт имеет свою историю.<br>
+            О ней и будет ниже.<br>
+          </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,7 +6,13 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: '/AboutSite', name: 'AboutSite', component: () => import('pages/AboutSite.vue') },
-      { path: '/Contact', name: 'Contact', component: () => import('pages/Contact.vue') }
+      { path: '/Contact', name: 'Contact', component: () => import('pages/Contact.vue') },
+      { path: '/Fanats', name: 'Fanats', component: () => import('pages/Fanats.vue') },
+      { path: '/Foto', name: 'Foto', component: () => import('pages/Foto.vue') },
+      { path: '/HistoryClub', name: 'HistoryClub', component: () => import('pages/HistoryClub.vue') },
+      { path: '/ResultMatch', name: 'ResultMatch', component: () => import('pages/ResultMatch.vue') },
+      { path: '/Stadium', name: 'Stadium', component: () => import('pages/Stadium.vue') },
+      { path: '/Video', name: 'Video', component: () => import('pages/Video.vue') }
     ]
   },
 
