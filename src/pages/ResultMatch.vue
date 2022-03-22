2f6ba187045255fb109417f8ef9352926e2b1a26
Denis Khodarev <lowdog136@gmail.com>
2022-03-22 08:40:28 +0200

    Оформление страницы "Результаты матчей"

--- src/pages/ResultMatch.vue
+++ src/pages/ResultMatch.vue
@@ -1,42 +1,58 @@
 <template>
-  <div class="q-pa-md">
-    <q-layout view="lHh lpr lFf" container style="height: 800px" class="shadow-2 rounded-borders">
-      <q-header elevated>
-        <q-toolbar>
-          <q-avatar>
-            <img  alt="logo" src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Товарищеский матч</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">Поражение</div>
+          <div class="text-caption text-grey">
+            ФК "Динамо" Санкт-Петербург - ФК "Север Мурманск" 4-2
+          </div>
+        </q-card-section>
 
-          <q-toolbar-title>
-            Результаты матчей
-          </q-toolbar-title>
+        <q-card-section class="col-5 flex flex-center">
+          <q-img
+            class="rounded-borders"
+            src="https://cdn.quasar.dev/img/parallax2.jpg"
+          />
+        </q-card-section>
+      </q-card-section>
+      <q-separator />
+    </q-card>
+    <q-card class="my-card" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-overline">Товарищеский матч</div>
+          <div class="text-h5 q-mt-sm q-mb-xs">Поражение</div>
+          <div class="text-caption text-grey">
+            ФК "Звезда" Санкт-Петербург - ФК "Север Мурманск" 6-1
+          </div>
+        </q-card-section>
 
-          <q-btn flat round dense icon="whatshot" />
-        </q-toolbar>
-      </q-header>
-
-      <q-page-container>
-        <q-page padding>
-          <p v-for="n in 1" :key="n">
-            13 марта 2022 <br>
-            Товарищеский матч <br>
-            ФК "Динамо" Санкт-Петербург - ФК "Север Мурманск" 4-2<br>
-          </p>
-          <p v-for="n in 1" :key="n">
-            19 марта 2022 <br>
-            Товарищеский матч <br>
-            ФК "Звезда" Санкт-Петербург - ФК "Север Мурманск" 6-1<br>
-          </p>
-        </q-page>
-      </q-page-container>
-    </q-layout>
+        <q-card-section class="col-5 flex flex-center">
+          <q-img
+            class="rounded-borders"
+            src="https://cdn.quasar.dev/img/parallax2.jpg"
+          />
+        </q-card-section>
+      </q-card-section>
+      <q-separator />
+    </q-card>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'PageIndex'
-})
+export default {
+  setup () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  }
+}
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 400px
+</style>
