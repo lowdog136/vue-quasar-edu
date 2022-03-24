2dfb028ac5e0eb12f11d32f54eeba4d2d614e35a
Denis Khodarev <lowdog136@gmail.com>
2022-03-24 11:56:17 +0200

    Тестовые блоки

---
+++ src/components/MyTestNewsCard.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-inner-loading
+      :showing="visible"
+      label="Please wait..."
+      label-class="text-teal"
+      label-style="font-size: 1.1em"
+    />
+    <!--    1 news -->
+    <q-card class="my-card" flat bordered>
+      <q-img :src="require('../assets/image/imgTitle/' + product_data.NewsClubNewsCardTitleUrlImg )" />
+
+      <q-card-section>
+        <div class="text-overline text-orange-9"> {{ product_data.NewsCardStatusMatch_F }}</div>
+        <div class="text-h5 q-mt-sm q-mb-xs">{{ product_data.NewsClubNewsCardTitleNews }}</div>
+        <div class="text-caption text-grey">
+          {{ product_data.NewsClubNewsCardPreViewNews }}
+        </div>
+        <div class="text-caption text-grey">
+          {{ product_data.NewsClubNewsCardCardNewsSrc }}
+        </div>
+      </q-card-section>
+
+      <q-card-actions>
+        <q-btn flat color="dark" label="" />
+        <div class="labelDate">
+          {{ product_data.NewsClubNewsCardDateNews }}
+        </div>
+<!--        <NewsCardDetailPopUp
+          :NewsClubNewsCardPopUpFullNews="NewsClubNewsCardFullNews"
+          :NewsClubNewsCardPopUpTitleUrlImg="NewsClubNewsCardTitleUrlImg"
+          :NewsClubNewsCardCardPopUpNewsSrc="NewsClubNewsCardCardNewsSrc"
+          :NewsClubNewsCardPopUpTitleNews="TitleNewsCard"/>-->
+        <q-space />
+
+        <q-btn
+          color="grey"
+          round
+          flat
+          dense
+          :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          @click="expanded = !expanded"
+        />
+      </q-card-actions>
+
+      <q-slide-transition>
+        <div v-show="expanded">
+          <q-separator />
+          <q-card-section class="text-subitle2">
+            {{ product_data.NewsClubNewsCardExtNews }}
+          </q-card-section>
+        </div>
+      </q-slide-transition>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'MyTestNewsCard',
+  components: {},
+  data () {
+    return {
+      dialog: ref(false),
+      cancelEnabled: ref(false),
+      expanded: ref(false),
+      dateNews222: '14.03.22'
+    }
+  },
+  props: {
+    product_data: {
+      type: Object,
+      default () {
+        return {
+          NewsClubNewsCardCardNewsSrc: String,
+          NewsClubNewsCardPreViewNews: String,
+          NewsClubNewsCardExtNews: String,
+          NewsClubNewsCardTitleUrlImg: String,
+          NewsCardStatusMatch_F: String,
+          NewsCardStatusMatch_O: String,
+          dateNews: String,
+          NewsClubNewsCardDateNews: String,
+          TitleNewsCard: String,
+          NewsClubNewsCardFullNews: String,
+          NewsClubNewsCardFullNews2: String,
+          NewsClubNewsCardTitleNews: String
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 350px
+</style>
---
+++ src/pages/MyTestNewsClub.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <MyTestNewsCard
+      v-for="NewsClubNewsCardS in $store.state.NewsClubNewsCard"
+      :key="NewsClubNewsCardS.id"
+      :product_data="NewsClubNewsCardS"
+    />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import MyTestNewsCard from 'components/MyTestNewsCard'
+
+export default {
+  components: { MyTestNewsCard },
+  data () {
+    return {}
+  },
+  setup () {
+    return {
+      expanded: ref(false)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 650px
+</style>
