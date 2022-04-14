79a869b77c4d0bbd7165e7e608ff0bdf139e01cc
Denis Khodarev <lowdog136@gmail.com>
2022-04-14 16:34:31 +0200

    Техническое

---
+++ src/components/ResultMatchAddData.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="NewsCardAnnounceNews"
+        label="Заголовок"
+        hint="Товарищеский матч, официальный матч"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Заполните поле']"
+      />
+      <q-input
+        filled
+        v-model="ResultCardTitle"
+        label="Результат игры"
+        hint="Победа, поражение, ничья"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Заполните поле']"
+      />
+      <q-input
+        filled
+        v-model="ResultCardTeam1"
+        label="Команда 1"
+        hint="ФК Имя Город"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Заполните поле']"
+      />
+      <q-input
+        filled
+        v-model="ResultCardTeam2"
+        label="Команда 2"
+        hint="ФК Имя Город"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Заполните поле']"
+      />
+      <q-input
+        filled
+        v-model="ResultCardResult"
+        label="Счет в матче"
+        hint="1-1 (1-0)"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Заполните поле']"
+      />
+
+      <div>
+        <q-btn @click="saveResultMatch" label="Отправить" type="submit" color="primary"/>
+        <q-btn label="Очистить" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+
+  </div>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
+export default {
+  name: 'ResultMatchAddData',
+  setup () {
+    const $q = useQuasar()
+
+    const name = ref(null)
+    const NewsCardAnnounceNews = ref(null)
+    const ResultCardTitle = ref(null)
+    const ResultCardTeam1 = ref(null)
+    const ResultCardTeam2 = ref(null)
+    const ResultCardResult = ref(null)
+    const accept = ref(false)
+
+    return {
+      name,
+      NewsCardAnnounceNews,
+      ResultCardTitle,
+      ResultCardTeam1,
+      ResultCardTeam2,
+      ResultCardResult,
+      accept,
+
+      onSubmit () {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'You need to accept the license and terms first'
+          })
+        } else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted'
+          })
+        }
+      },
+
+      onReset () {
+        name.value = null
+        NewsCardAnnounceNews.value = null
+        ResultCardTitle.value = null
+        ResultCardTeam1.value = null
+        ResultCardTeam2.value = null
+        ResultCardResult.value = null
+        accept.value = false
+      },
+
+      saveResultMatch () {
+        console.log('saveResultMatch')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
