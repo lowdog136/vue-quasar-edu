89623c4e31e4d2ef898fc9e387071e7f94956931
Denis Khodarev <lowdog136@gmail.com>
2022-05-15 20:55:20 +0200

    Admin panel. Visual style

--- src/components/MyTestNewsCard.vue
+++ src/components/MyTestNewsCard.vue
@@ -8,8 +8,8 @@
       />
 
       <q-card-section>
-        <div class="text-overline text-orange-9">{{ item.title }}</div>
-        <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
+        <div class="text-overline text-orange-9">{{ item.preview }}</div>
+        <div class="text-h5 q-mt-sm q-mb-xs">{{ item.title }}</div>
         <div class="text-caption text-grey">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </div>
@@ -70,6 +70,7 @@ export default {
       itemBody: '',
       itemTitle: '',
       itemStatus: '',
+      itemNewsClubNewsCardTitleNews: '',
       itemTeam1: '',
       itemTeam2: '',
       itemResult: ''
--- src/components/ResultMatchAddDataTest.vue
+++ src/components/ResultMatchAddDataTest.vue
@@ -2,23 +2,37 @@
   <div class="container">
     <div id="app">
       <h4>Добавить новость:</h4>
-      <p> Статус новости. Показывать или нет
+      <q-separator/>
+      <p> Статус новости. Показывать или нет<br/>
         <input v-model="itemNewsClubNewsCardStatus" type="text" placeholder="false or true"/><br/>
       </p>
-      <p>предЗаголовок - победа,ничья, поражение, анонс
+      <p>предЗаголовок - победа,ничья, поражение, анонс<br/>
         <input v-model="itemNewsCardAnnounceNews" type="text" placeholder="itemTitle"/><br />
       </p>
-      <input v-model="itemTitle" type="text" placeholder="itemTitle"/><br />
-      <input v-model="itemStatus" type="text" placeholder="itemStatus"/><br />
-      <input v-model="itemTeam1" type="text" placeholder="itemTeam1"/><br />
-      <input v-model="itemTeam2" type="text" placeholder="itemTeam2"/><br />
-      <input v-model="itemResult" type="text" placeholder="itemResult"/><br />
-      <input v-model="itemTitle" type="text" placeholder="itemTitle"/><br />
-      <input v-model="itemTitle" type="text" placeholder="itemTitle"/><br />
-      <input v-model="itemStatus" type="text" placeholder="itemStatus"/><br />
-      <input v-model="itemTeam1" type="text" placeholder="itemTeam1"/><br />
-      <input v-model="itemTeam2" type="text" placeholder="itemTeam2"/><br />
-      <input v-model="itemResult" type="text" placeholder="itemResult"/><br />
+      <p>Заголовок новости<br/>
+        <input v-model="itemNewsClubNewsCardTitleNews" type="text" placeholder="itemTitle"/>
+      </p>
+      <p>предТекстНовости - превью. 2-3 предложения<br/>
+        <input v-model="itemNewsClubNewsCardPreViewNews" type="text" placeholder="itemStatus"/>
+      </p>
+      <p>Текст - основной текст. полный. работает по кнопке подробно<br/>
+        <input v-model="itemNewsClubNewsCardFullNews" type="text" placeholder="itemTitle"/>
+      </p>
+      <p>раскрТекст - текст показывающий при раскрытии.чуть больше чем в предТекстНовости<br/>
+        <input v-model="itemNewsClubNewsCardExtNews" type="text" placeholder="itemTitle"/>
+      </p>
+      <p>датаНовости<br />
+        <input v-model="itemNewsClubNewsCardDateNews" type="date" placeholder="датаНовости"/>
+      </p>
+      <p>источник новости<br />
+        <input v-model="itemNewsClubNewsCardCardNewsSrc" type="url" placeholder="датаНовости"/>
+      </p>
+      <q-separator/>
+      <p> Данные для страницы с итогами матча<br />
+        <input v-model="itemTeam1" type="text" placeholder="itemTeam1"/><q-space/>
+        <input v-model="itemTeam2" type="text" placeholder="itemTeam2"/><q-space/>
+        <input v-model="itemResult" type="text" placeholder="itemResult"/><br />
+      </p>
       <button @click="addPost()">Add Item</button> <br>
     </div>
     <ul>
@@ -28,10 +42,15 @@
         :key="item.id"
         @click="boughtItem(item.id)"
         @dblclick="removeItem(item.id)"
-      ><br>
+      ><br><q-separator/>
         <h4>Удалить новость: {{ item.id }}</h4>
         itemTitle: {{ item.title }} <br>
         itemStatus: {{ item.status }} <br>
+        item.preview: {{ item.preview }} <br>
+        item.fullnews: {{ item.fullnews }} <br>
+        item.extnews: {{ item.extnews }} <br>
+        item.datenews: {{ item.datenews }} <br>
+        item.srcnews: {{ item.srcnews }} <br>
         itemTeam1: {{ item.team1 }} <br>
         itemTeam2: {{ item.team2 }} <br>
         itemResult: {{ item.result }} <br>
@@ -49,9 +68,14 @@ export default {
       items: [],
       itemName: '',
       itemBody: '',
-      itemTitle: '',
+      itemNewsClubNewsCardFullNews: '',
+      itemNewsClubNewsCardPreViewNews: '',
+      itemNewsClubNewsCardTitleNews: '',
       itemNewsCardAnnounceNews: '',
+      itemNewsClubNewsCardExtNews: '',
       itemNewsClubNewsCardStatus: '',
+      itemNewsClubNewsCardDateNews: '',
+      itemNewsClubNewsCardCardNewsSrc: '',
       itemTeam1: '',
       itemTeam2: '',
       itemResult: ''
@@ -85,9 +109,13 @@ export default {
       const res = await axios.post('http://localhost:3000/items', {
         name: this.itemName,
         body: this.itemBody,
-        title: this.itemTitle,
+        title: this.itemNewsClubNewsCardTitleNews,
         status: this.itemNewsClubNewsCardStatus,
         preview: this.itemNewsCardAnnounceNews,
+        fullnews: this.itemNewsClubNewsCardFullNews,
+        extnews: this.itemNewsClubNewsCardExtNews,
+        datenews: this.itemNewsClubNewsCardDateNews,
+        srcnews: this.itemNewsClubNewsCardCardNewsSrc,
         team1: this.itemTeam1,
         team2: this.itemTeam2,
         result: this.itemResult
@@ -95,9 +123,13 @@ export default {
       this.items = [...this.items, res.data]
       this.itemName = ''
       this.itemBody = ''
-      this.itemTitle = ''
+      this.itemNewsClubNewsCardFullNews = ''
+      this.itemNewsClubNewsCardTitleNews = ''
       this.itemNewsClubNewsCardStatus = ''
       this.itemNewsCardAnnounceNews = ''
+      this.itemNewsClubNewsCardDateNews = ''
+      this.itemNewsClubNewsCardExtNews = ''
+      this.itemNewsClubNewsCardCardNewsSrc = ''
       this.itemTeam1 = ''
       this.itemTeam2 = ''
       this.itemResult = ''
