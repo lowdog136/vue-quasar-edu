4360fdb3ce5165260f36bf15ba0facc95d56425a
Denis Khodarev <lowdog136@gmail.com>
2022-06-14 21:08:29 +0200

    ResultMatchResultCardAddDataAdmin.vue done

--- src/components/ResultMatchCard.vue
+++ src/components/ResultMatchCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="q-pa-md row items-start q-gutter-md"
-  v-for="resultGame in resultGames.slice(id).reverse()"
-  :key="resultGame.id">
+       v-for="resultGame in resultGames.slice(id).reverse()"
+       :key="resultGame.id">
     <q-card class="my-card" flat bordered>
-      <q-img :src="require('../assets/image/imgTitle/title_0.png' )" />
+      <q-img width="70%" height="80%" :src="require('../assets/image/imgTitle/title_0.png' )" />
       <q-card-section>
         <div class="text-overline text-orange-9">{{ resultGame.title }}</div>
         <div class="text-caption text-grey">{{ resultGame.date }}</div>
@@ -66,5 +66,4 @@ export default {
   height: 60px
 .my-card
   display: flex
-  width: 300px
 </style>
