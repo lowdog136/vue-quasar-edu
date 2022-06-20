657a7ba1c4b1da5f549df641555c481689ca8099
Denis Khodarev <lowdog136@gmail.com>
2022-06-20 21:19:00 +0200

    Table event component

--- src/components/Test2Comp.vue
+++ src/components/Test2Comp.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="q-px-lg q-pb-md">
-    <q-timeline :layout="layout" color="secondary">
-      <q-timeline-entry heading>Июнь, 2022</q-timeline-entry>
+    <q-timeline :layout="layout" color="secondary" v-for="test in tests" :key="test.id">
+      <q-timeline-entry heading>{{ test.mounth }}</q-timeline-entry>
         <q-timeline-entry
-          v-for=" test in tests" :key="test.id"
+          v-for="matchEvent in test.matchEvents" :key="matchEvent.id"
           :title='titleEvent'
-          subtitle="Июнь 1, 2022"
+          :subtitle=matchEvent.data
           side="left"
-          color="orange-14"
-          icon="done_all"
+          :color=matchEvent.color
+          :icon=matchEvent.icon
         >
           <div>
-            {{ test.tour }} {{ test.other }}
+            {{ matchEvent.tour}}<br/>{{ matchEvent.other}}
           </div>
         </q-timeline-entry>
     </q-timeline>
@@ -29,50 +29,93 @@ export default {
       tests: [
         {
           id: 1,
-          tour: '1 tour',
-          other: 'СШ "Ленинградец" 1 - 2 ФК "Север"'
+          mounth: 'May',
+          matchEvents: [
+            {
+              id: 1,
+              tour: '1 tour',
+              other: 'СШ "Ленинградец" 6 - 2 ФК "Север"',
+              color: 'primary',
+              data: 'май 1, 2022',
+              mounth: 'июнь',
+              icon: 'bookmark_border'
+            },
+            {
+              id: 2,
+              tour: '2 tour',
+              other: 'СШ "Ленинградец" 2 - 2 ФК "Север"',
+              color: 'primary',
+              data: 'май 15, 2022',
+              mounth: 'июнь',
+              icon: 'bookmark_border'
+            },
+            {
+              id: 3,
+              tour: '3 tour',
+              other: 'СШ "Ленинградец" 1 - 2 ФК "Север"',
+              color: 'primary',
+              data: 'май 25, 2022',
+              mounth: 'июнь',
+              icon: 'bookmark_border'
+            }
+          ]
         },
         {
           id: 2,
-          tour: '2 tour',
-          other: 'СШ "Ленинградец" 2 - 2 ФК "Север"'
+          mounth: 'June',
+          matchEvents: [
+            {
+              id: 1,
+              tour: '4 tour',
+              other: 'СШ "Ленинградец" 6 - 2 ФК "Север"',
+              color: 'primary',
+              data: 'June 1, 2022',
+              mounth: 'June',
+              icon: 'bookmark_border'
+            },
+            {
+              id: 2,
+              tour: '5 tour',
+              other: 'СШ "Ленинградец" 2 - 2 ФК "Север"',
+              color: 'primary',
+              data: 'June 11, 2022',
+              mounth: 'June',
+              icon: 'bookmark_border'
+            },
+            {
+              id: 3,
+              tour: '6 tour',
+              other: 'СШ "Ленинградец" 1 - 2 ФК "Север"',
+              color: 'primary',
+              data: 'June 31, 2022',
+              mounth: 'June',
+              icon: 'bookmark_border'
+            }
+          ]
         },
         {
           id: 3,
-          tour: '3 tour',
-          other: ''
-        },
-        {
-          id: 4,
-          tour: '4 tour',
-          other: ''
-        },
-        {
-          id: 5,
-          tour: '5 tour',
-          other: ''
-        }
-      ],
-      matches: [
-        {
-          id: 1,
-          match: 'СШ "Ленинградец" 1 - 2 ФК "Север"'
-        },
-        {
-          id: 2,
-          match: 'СШ "Ленинградец" 2 - 2 ФК "Север"'
-        },
-        {
-          id: 3,
-          match: 'СШ "Ленинградец" 3 - 2 ФК "Север"'
-        },
-        {
-          id: 4,
-          match: 'СШ "Ленинградец" 4 - 2 ФК "Север"'
-        },
-        {
-          id: 5,
-          match: 'СШ "Ленинградец" 5 - 2 ФК "Север"'
+          mounth: 'Jule',
+          matchEvents: [
+            {
+              id: 1,
+              tour: '7 tour',
+              other: 'СШ "Ленинградец" 6 - 2 ФК "Север"',
+              color: 'primary',
+              data: 'май 1, 2022',
+              mounth: 'Jule',
+              icon: 'bookmark_border'
+            },
+            {
+              id: 2,
+              tour: '8 tour',
+              other: 'СШ "Ленинградец" 2 - 2 ФК "Север"',
+              color: 'primary',
+              data: 'май 15, 2022',
+              mounth: 'Jule',
+              icon: 'bookmark_border'
+            }
+          ]
         }
       ]
     }
@@ -101,6 +144,13 @@ export default {
           ]
         }
       ]
+    }
+  },
+  computed: {
+    evenNumbers: function () {
+      return this.mounth.filter(function (mounth) {
+        return mounth
+      })
     }
   },
   props: {
