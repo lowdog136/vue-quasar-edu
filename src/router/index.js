15148c5d1c70403404738abcfa723514c64605ff
Denis Khodarev <lowdog136@gmail.com>
2022-06-21 16:44:09 +0200

    tests

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,19 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
   })
+  /*  Router.beforeEach((to, from, next) => {
+    if (to.path === '/login' && auth.currentUser) {
+      next('/')
+      return
+    }
+
+    if (to.matched.some(record => record.meta.requiresAuth) && !auth.currentUser) {
+      next('/Admin')
+      return
+    }
+
+    next()
+  }) */
 
   return Router
 })
