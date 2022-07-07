f6e974e4d97149397e7e80576da424ee1fe7d388
Denis Khodarev <lowdog136@gmail.com>
2022-07-07 16:57:43 +0200

    add select mode

---
+++ src/components/Selects/NewsCardSubTitleSelect.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-select clearable filled color="purple-12" v-model="model" :options="options" label="Label" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'NewsCardSubTitleSelect',
+  setup: function () {
+    return {
+      model: ref('Новости клуба'),
+
+      options: [
+        'Новости клуба', 'Результат матча', 'Анонс матча'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
