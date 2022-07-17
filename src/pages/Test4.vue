b17e2a19137ce9a19df38736cf096d8b8787c4ee
Denis Khodarev <lowdog136@gmail.com>
2022-07-17 20:42:46 +0200

    page module test

--- src/pages/Test4.vue
+++ src/pages/Test4.vue
@@ -97,9 +97,26 @@
     </div>
   </div>
   <div>
+    <q-btn @click="pageCount--" icon="chevron_left"/>
+    {{ pageCount }}
+    <q-btn @click="pageCount++" icon="chevron_right"/>
     <h5>text herer 2</h5>
-    <div v-for="text in texts" :key="text.length">
-      {{ text }}
+    <div       v-for="text in texts"
+               :key="text.id">
+      <q-card
+        v-show="pageCount == text.id"
+        class="my-card text-white"
+        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+      >
+        <q-card-section>
+          <div class="text-h6">{{  text.title }}</div>
+          <div class="text-subtitle2">by {{ text.author }}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          {{ text.body }}
+        </q-card-section>
+      </q-card>
     </div>
   </div>
 </template>
@@ -154,7 +171,12 @@ export default {
     }
   },
   setup () {
-    const texts = ref('12 33 44 55')
+    const texts = ref([
+      { id: 1, title: 'My 1 title', author: 'B.J.Greys', body: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"\n' },
+      { id: 2, title: 'My 2 title', author: 'A.Brown', body: '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"\n' },
+      { id: 3, title: 'My 3 title', author: 'W.Loyds', body: '"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."\n' },
+      { id: 4, title: 'My 4 title', author: 'S.K.J.Mouse', body: '"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?""\n' }])
+    const pageCount = ref('1')
     const listCount = ref('')
     const activeCard = ref('')
     const todos = ref([])
@@ -251,6 +273,7 @@ export default {
       updateTeam1,
       updateTeam2,
       addEvent,
+      pageCount,
       activeCard,
       listCount,
       events,
@@ -282,7 +305,8 @@ export default {
 <style lang="sass" scoped>
 .my-card
   width: 100%
-  max-width: 450px
+  max-width: 550px
+  background: #2c3e50
   .root
     width: 400px
     margin: 0 auto
