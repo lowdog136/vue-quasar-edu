de010499e0d4c0598b70e10ec4f46d9be73e809e
lowdog136 <lowdog136@gmail.com>
2022-11-18 19:05:05 +0700

    admin mode: add comp AdminPageEditList.vue

---
+++ src/components/Admin/AdminPageEditList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-item >
+      <q-item-section avatar>
+        <q-icon color="primary" name="edit" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Редактировать архив игр:</q-item-label>
+        <q-item-label caption>
+          Set the content filtering level to restrict
+          apps that can be downloaded
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminPageEditList'
+}
+</script>
+
+<style scoped>
+
+</style>
