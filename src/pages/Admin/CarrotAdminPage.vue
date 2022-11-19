4a525d9569b9e83d82f50afc2b64a819b49bb751
Denis Khodarev <lowdog136@gmail.com>
2022-11-19 07:08:40 +0200

    remove two mode (newsSite/newsClub)

--- src/pages/Admin/CarrotAdminPage.vue
+++ src/pages/Admin/CarrotAdminPage.vue
@@ -11,46 +11,15 @@
             CarrotAdminPage (experiment panel)
           </q-toolbar-title>
           <q-btn to="/" color="black" flat round dense icon="first_page" />
-          <q-toggle
-            :false-value="false"
-            :true-value="true"
-            color="white"
-            v-model="panelViewSiteUpdate"
-            icon="sync_alt"
-          />
           <q-btn @click="panelViewOnEventCard" @dblclick="panelViewOffEventCard" color="white" flat round dense icon="event" />
           <q-btn @click="panelViewOnResultCard" @dblclick="panelViewOffResultCard" color="white" flat round dense icon="smart_toy" />
-          <q-toggle
-            :false-value="false"
-            :true-value="true"
-            color="white"
-            v-model="panelViewNewsCard"
-            icon="help"
-          />
         </q-toolbar>
       </q-header>
-      <!--      Показать админку добавление обновления сайта-->
-      <q-page-container v-if="panelViewSiteUpdate">
-        <q-page padding>
-          <p v-for="n in 1" :key="n">
-            <NewsSiteContentAdd />
-          </p>
-        </q-page>
-      </q-page-container>
       <!--      Показать админку добавление событий-->
       <q-page-container v-if="panelViewEvent">
         <q-page padding>
           <p v-for="n in 1" :key="n">
             <GamesNowEventAdd />
-          </p>
-        </q-page>
-      </q-page-container>
-      <!--      Показать админку добавление новостей-->
-      <q-page-container v-if="panelViewNewsCard">
-        <q-page padding>
-          <p v-for="n in 1" :key="n">
-            <NewsCardContentAdd />
-      <!--            <ResultMatchAddDataAdmin />-->
           </p>
         </q-page>
       </q-page-container>
@@ -76,14 +45,12 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import NewsCardContentAdd from 'components/Admin/NewsCardContentAdd'
 import ResultMatchResultCardAddDataAdmin from 'components/ResultMatchResultCardAddDataAdmin'
-import NewsSiteContentAdd from 'components/Admin/NewsSiteContentAdd'
 import GamesNowEventAdd from 'components/Admin/GamesNowEventAdd'
 
 export default defineComponent({
   name: 'CarrotAdminPage',
-  components: { NewsCardContentAdd, ResultMatchResultCardAddDataAdmin, GamesNowEventAdd, NewsSiteContentAdd },
+  components: { ResultMatchResultCardAddDataAdmin, GamesNowEventAdd },
   data () {
     return {
       panelSiteUpdate: false,
