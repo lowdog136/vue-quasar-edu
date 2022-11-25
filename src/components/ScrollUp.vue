0b5af6d029859ec5e8cbeb47880efa2130499496
lowdog136 <lowdog136@gmail.com>
2022-11-25 16:16:45 +0700

    new comp ScrollUp.vue

---
+++ src/components/ScrollUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="q-pa-md">
+    <q-page-scroller expand position="top" :scroll-offset="150" :offset="[0, 0]">
+      <div class="col cursor-pointer q-pa-sm bg-black text-white text-center">
+        Вверх...
+      </div>
+    </q-page-scroller>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrollUp'
+}
+</script>
+
+<style scoped>
+
+</style>
