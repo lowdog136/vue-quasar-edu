8b91ca2f1aee771b6d0680b8527b754adc5e3ba8
zooloo <lowdog136@gmail.com>
2023-06-05 20:32:06 +0200

    test date fb

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,25 +1,22 @@
 <template>
-  <div>
-    <q-item-label v-for="NewsCard in GamesNowEvents"
-                  :key="NewsCard.id" caption>
-      <span class="q-px-sm bg-deep-orange text-white text-italic rounded-borders">{{ NewsCard.title }}</span>
-    </q-item-label>
-    <q-item-label v-for="NewsCard in GamesNowEvents"
-                  :key="NewsCard.id" caption>
-      <span class="q-px-sm bg-deep-orange text-white text-italic rounded-borders">{{ NewsCard.title }}</span>
-    </q-item-label>
+  <div class="q-pa-md">
+    <div>
+      <q-item-label v-for="NewsCard in GamesNowEvents"
+                    :key="NewsCard.id" caption>
+        <span class="q-px-sm bg-deep-orange text-white text-italic rounded-borders">{{ NewsCard.datenews }}</span>
+        <span class="q-px-sm">{{ NewsCard.title }}</span>
+      </q-item-label>
+        <q-item-label v-for="NewsCard in GamesNowEvents"
+                      :key="NewsCard.id" caption>
+        </q-item-label>
+      <span class="q-px-sm bg-deep-orange text-white text-italic rounded-borders">{{ NewTitle[0] }}</span>
+    </div>
   </div>
 </template>
 <script>
 import { onMounted, ref } from 'vue'
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { collection, onSnapshot } from 'firebase/firestore'
 import { db } from 'src/firebase'
-// раздел "Календарь игр"
-const eventCollectionRefGamesNowEvent = collection(db, '/siteNewsDrawer/GamesNowEvent/dateUpdate/')
-const eventCollectionQueryGamesNowEvent = query(eventCollectionRefGamesNowEvent, orderBy('date', 'desc'))
-// раздел "Турнирная таблица"
-const eventCollectionRefGamesNowTable = collection(db, 'siteNewsDrawer/')
-const eventCollectionQueryGamesNowTable = query(eventCollectionRefGamesNowTable, orderBy('date', 'desc'))
 export default {
   name: 'NewsDrawer',
   setup () {
@@ -27,9 +24,10 @@ export default {
     const NewsCards = ref([])
     const GamesNowEvents = ref([])
     const GamesNowTables = ref([])
+    const NewTitle = ref([GamesNowEvents])
     onMounted(async () => {
       // раздел "Календарь игр"
-      onSnapshot(eventCollectionQueryGamesNowEvent, (querySnapshot) => {
+      onSnapshot(collection(db, '/siteNewsDrawer/GamesNowTest/dateUpdate/'), (querySnapshot) => {
         const fbGamesNowEvents = []
         querySnapshot.forEach((doc) => {
           const GamesNowEvent = {
@@ -41,21 +39,11 @@ export default {
         GamesNowEvents.value = fbGamesNowEvents
         console.log('Дата обновления календаря игр', GamesNowEvents)
       })
-      // раздел "Турнирная таблица"
-      onSnapshot(eventCollectionQueryGamesNowTable, (querySnapshot) => {
-        const fbGamesNowTables = []
-        querySnapshot.forEach((doc) => {
-          const GamesNowTable = {
-            datenews: doc.data().datenews
-          }
-          fbGamesNowTables.push(GamesNowTable)
-        })
-        GamesNowTables.value = fbGamesNowTables
-      })
     })
     return {
       labelAboutSite: 'О сайте',
       NewsCards,
+      NewTitle,
       GamesNowEvents,
       GamesNowTables,
       events
