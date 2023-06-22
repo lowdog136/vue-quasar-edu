ae0169c80c7da9db5b05395720ab96a7d93120c9
zooloo <lowdog136@gmail.com>
2023-06-22 20:47:12 +0200

    test siteUpdate new design - add

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,172 +1,207 @@
 <template>
-  <div class="q-pa-lg">
-    <div v-if="$store.state.user">
-      <p>
-        title 1/1
-      </p>
-      <p>
-        title 1/2
-      </p>
-    </div>
-    <div>
-      <p>
-        title 2
-      </p>
-    </div>
-    <q-btn flat dense round icon="done_all" @click="dialogLogOut = true" />
-    <q-dialog v-model="dialogLogOut">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">До свидания, {{ $store.getters.userName }} </div>
-        </q-card-section>
-        <q-card-section class="row items-center q-gutter-sm">
-          <q-btn v-close-popup label="Выйти ?" to="/" @click="$store.dispatch('logout')" color="primary" />
-        </q-card-section>
-      </q-card>
-    </q-dialog>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-list style="max-width: 600px">
+      <q-item-label header>Хронология обновлений</q-item-label>
+
+      <q-item v-for="event in events" :key="event.id">
+        <q-item-section avatar top>
+          <q-icon name="account_tree" color="black" size="34px" />
+        </q-item-section>
+        <q-item-section top class="col-3 gt-sm">
+          <q-item-label class="q-mt-sm">{{ event.dateupd }}</q-item-label>
+        </q-item-section>
+        <q-item-section top>
+          <q-item-label lines="1">
+            <span class="text-weight-medium">{{ event.ver }}</span>
+            <span class="text-grey-8"> - {{ event.title }}</span>
+          </q-item-label>
+          <q-item-label caption lines="1">
+            {{ event.body }}
+          </q-item-label>
+          <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase">
+            <span class="cursor-pointer">Open in GitHub</span>
+          </q-item-label>
+          <q-separator spaced />
+        </q-item-section>
+      </q-item>
+      <q-separator spaced />
+    </q-list>
+    <q-timeline color="secondary" >
+      <q-timeline-entry heading >
+        Хронология обновлений
+      </q-timeline-entry>
+      <q-timeline-entry v-for="event in events" :key="event.id"
+                        :title=event.title
+                        :subtitle=event.dateupd
+                        icon="done"
+      >
+        <div>
+          ver: {{ event.ver }}
+        </div>
+        <div style="max-width: 650px">
+          {{ event.body }}
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
+    <ScrollUp />
   </div>
 </template>
 
 <script>
-import { useQuasar } from 'quasar'
-import { computed, onMounted, ref } from 'vue'
-import { collection, onSnapshot, orderBy } from 'firebase/firestore'
-import { db } from 'src/firebase'
+import { ref, onMounted } from 'vue'
+import { mapActions, mapGetters } from 'vuex'
+import { collection, onSnapshot, addDoc, doc, deleteDoc, updateDoc, query, orderBy } from 'firebase/firestore'
+import { db } from '../firebase'
+import ScrollUp from 'components/ScrollUp'
 
-// import NewsCardDetailPopUp from 'components/NewsCardDetailPopUp'
-// const newsCardCollectionRef = collection(db, 'events', 'polpred')
-// const newsCardCollectionQuery = query(newsCardCollectionRef, orderBy('date', 'desc'))
+const eventCollectionRef = collection(db, 'siteUpdates')
+const eventCollectionQuery = query(eventCollectionRef, orderBy('date', 'desc'))
+const newEventVer = ref('')
+const newEventTitle = ref('')
+const newEventBody = ref('')
+const newEventDateUpd = ref('')
+const newEventDate = ref('')
+const newEventCount = ref('')
 
-// import NewsCardDetailPopUp from 'components/NewsCardDetailPopUp'
+const addEvent = () => {
+  addDoc(eventCollectionRef, {
+    ver: newEventVer.value,
+    title: newEventTitle.value,
+    body: newEventBody.value,
+    dateupd: newEventDateUpd.value,
+    date: Date.now(),
+    done: true
+  })
+  newEventVer.value = ''
+  newEventTitle.value = ''
+  newEventBody.value = ''
+  newEventDateUpd.value = ''
+  newEventDate.value = ''
+  newEventCount.value = ''
+  console.log('add todo', newEventDate.value)
+}
+
+const deleteEvent = id => {
+  deleteDoc(doc(eventCollectionRef, id))
+}
 
 export default {
-  name: 'eventSZFO23',
-  components: {},
+  name: 'siteUpdate',
+  components: { ScrollUp },
   data () {
-    return {
-      btnColor: 'blue-grey-10',
-      btnName: 'Подробно',
-      // кнопка что бы не прилипала к краю
-      btnDivMain: '',
-      divclassFootTab: 'q-pa-m'
-    }
+    return {}
   },
   setup () {
-    const $q = useQuasar()
-    const matchEvents = ref([])
-    const NewsCards = ref([])
-    const NewsCardsJ = ref([])
-    const NewsCardsJl = ref([])
-    const NewsCardsA = ref([])
+    const todos = ref([])
+    const events = ref([])
     onMounted(async () => {
-      // NewsCard Module
-      onSnapshot(collection(db, '/events/szfo/2023/may/match'), orderBy('date', 'desc'), (querySnapshot) => {
-        const fbEvents = []
+      onSnapshot(eventCollectionQuery, (querySnapshot) => {
+        const fbTodos = []
         querySnapshot.forEach((doc) => {
-          const listDateEvent = {
+          const todo = {
             id: doc.id,
+            content: doc.data().content,
             title: doc.data().title,
-            color: doc.data().color,
-            date: doc.data().date,
-            mounth: doc.data().mounth,
-            tour: doc.data().tour,
-            eventName: doc.data().eventName,
-            time: doc.data().time,
-            icon: doc.data().icon,
-            scorer: doc.data().scorer
+            done: doc.data().done
           }
-          fbEvents.push(listDateEvent)
+          fbTodos.push(todo)
         })
-        NewsCards.value = fbEvents
-        console.log(NewsCards)
+        todos.value = fbTodos
       })
-      onSnapshot(collection(db, '/events/szfo/2023/june/match'), orderBy('date', 'desc'), (querySnapshot) => {
+      onSnapshot(eventCollectionQuery, (querySnapshot) => {
         const fbEvents = []
         querySnapshot.forEach((doc) => {
-          const listDateEvent = {
+          const event = {
             id: doc.id,
+            ver: doc.data().ver,
             title: doc.data().title,
-            color: doc.data().color,
+            body: doc.data().body,
+            dateupd: doc.data().dateupd,
             date: doc.data().date,
-            mounth: doc.data().mounth,
-            tour: doc.data().tour,
-            eventName: doc.data().eventName,
-            time: doc.data().time,
-            icon: doc.data().icon,
-            scorer: doc.data().scorer
+            done: doc.data().done
           }
-          fbEvents.push(listDateEvent)
+          fbEvents.push(event)
+          console.log('body-', event.body)
         })
-        NewsCardsJ.value = fbEvents
-        console.log(NewsCardsJ)
-      })
-      onSnapshot(collection(db, '/events/szfo/2023/jule/match'), orderBy('date', 'desc'), (querySnapshot) => {
-        const fbEvents = []
-        querySnapshot.forEach((doc) => {
-          const listDateEvent = {
-            id: doc.id,
-            title: doc.data().title,
-            color: doc.data().color,
-            date: doc.data().date,
-            mounth: doc.data().mounth,
-            tour: doc.data().tour,
-            eventName: doc.data().eventName,
-            time: doc.data().time,
-            icon: doc.data().icon,
-            scorer: doc.data().scorer
-          }
-          fbEvents.push(listDateEvent)
-        })
-        NewsCardsJl.value = fbEvents
-        console.log(NewsCardsJl)
-      })
-      onSnapshot(collection(db, '/events/szfo/2023/april/match'), orderBy('date', 'desc'), (querySnapshot) => {
-        const fbEvents = []
-        querySnapshot.forEach((doc) => {
-          const listDateEvent = {
-            id: doc.id,
-            title: doc.data().title,
-            color: doc.data().color,
-            date: doc.data().date,
-            mounth: doc.data().mounth,
-            tour: doc.data().tour,
-            eventName: doc.data().eventName,
-            time: doc.data().time,
-            icon: doc.data().icon,
-            scorer: doc.data().scorer
-          }
-          fbEvents.push(listDateEvent)
-        })
-        NewsCardsA.value = fbEvents
-        console.log(NewsCardsA)
+        events.value = fbEvents
       })
     })
-
-    return {
-      titleEvent: ['XXIV Турнир полпреда СЗФО'],
-      titleEventTest: ['1', '2', '3'],
-      titleEvent1: 'TitileEventValue1',
-      btnSize: 'xs',
-      NewsCards,
-      NewsCardsJ,
-      NewsCardsJl,
-      NewsCardsA,
-      matchEvents,
-      titleMainEvent: '',
-      layout: computed(() => {
-        return $q.screen.lt.sm ? 'dense' : ($q.screen.lt.md ? 'comfortable' : 'loose')
+    const toggleEvent = id => {
+      const index = events.value.findIndex(event => event.id === id)
+      updateDoc(doc(eventCollectionRef, id), {
+        done: !events.value[index].done
       })
     }
-  }
+    const countUpEvent = id => {
+      const index = events.value.findIndex(event => event.id === id)
+      updateDoc(doc(eventCollectionRef, id), {
+        count: events.value[index].count++
+      })
+      console.log('countUP', events.value[index].count)
+    }
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      titleMainEvent: 'samething title2',
+      newEventVer,
+      newEventTitle,
+      newEventBody,
+      newEventDateUpd,
+      newEventCount,
+      done: ref(true),
+      redModel: ref(false),
+      deleteEvent,
+      deleteDoc,
+      toggleEvent,
+      countUpEvent,
+      addEvent,
+      events,
+      todos,
+      tab: ref(['alarms', 'mails']),
+      expanded: ref(false)
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'dropDown'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'togledropDown',
+      'changePush',
+      'myCountZero',
+      'myCountUp',
+      'howWatch'
+    ])
+  },
+
+  props: {}
 }
 </script>
 
 <style lang="sass" scoped>
-.title
-  font-size: 28px
-  text-align: center
-  color: #2c3e50
 .my-card
   width: 100%
-  max-width: 350px
+  max-width: 450px
+  .root
+    width: 400px
+    margin: 0 auto
+    background-color: #fff
+    padding: 30px
+    margin-top: 100px
+    border-radius: 20px
+  input
+    border: none
+    outline: none
+    border-bottom: 1px solid #ddd
+    font-size: 1em
+    padding: 5px 0
+    margin: 10px 0 5px 0
+    width: 100%
+  button
+    background-color: #3498db
+    padding: 10px 20px
+    margin-top: 10px
+    border: none
+    color: white
 </style>
