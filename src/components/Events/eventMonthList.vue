63a15c6d5c66e85772b7c940f95bee11d581d079
zooloo <lowdog136@gmail.com>
2023-06-28 20:36:19 +0200

    add and test new comp

--- src/components/Events/eventMonthList.vue
+++ src/components/Events/eventMonthList.vue
@@ -1,6 +1,65 @@
 <template>
   <div class="q-pa-sm wrap rounded-borders">
-    <eventMonthList />
+    <div v-for="list in monthList" :key="list.id">
+      {{ list.id }}
+    </div>
+    <div v-for="item in NewsCardsA" :key="item.id">
+      {{ item.title }}
+    </div>
+    <q-item>
+      <q-timeline :layout="layout" color="secondary">
+        <q-timeline-entry heading v-for="list in monthList" :key="list.id">
+          <div class="title" style="color: #ae0000">
+            {{ list.id }}, 2023
+          </div>
+        </q-timeline-entry>
+        <q-timeline-entry v-for="item in NewsCardsA" :key="item.id"
+                          :title="item.eventName"
+                          :subtitle="item.date"
+                          :color="item.color"
+                          :icon="item.icon"
+                          side="left"
+        >
+          <div>
+            {{ item.tour}}
+            {{ item.mounth}}
+          </div>
+          <div>
+            <!--          {{ item.title }}-->
+            <q-item>
+              <q-item-section>
+                <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-primary ">
+                  {{ item.title }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item >
+              <q-item-section >
+                <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-blue-grey-5 ">
+                  голы: {{ item.scorer }}
+                </q-item-label>
+                <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-grey-5 ">
+                  планируют посетить: 0
+                </q-item-label>
+                <q-item-label lines="1" class="q-pt-xs text-body2 text-weight-bold text-primary text-uppercase">
+                <span class="cursor-pointer">
+                  <NewsCardDetailPopUp
+                    :PopyUpSubTitleNews="item.event"
+                    :PopyUpFullNews="item.body"
+                    :PopyUpTitleNews="item.title"
+                    :PopyUpBtnColor="btnColor"
+                    :PopyUpBtnName="btnName"
+                    :PopyUpDivMain = "btnDivMain"
+                  /></span>
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+            <!--          <q-item-label caption>голы: {{ item.scorer }} </q-item-label>-->
+            <!--          <q-item-label caption>планируют посетить: 0 </q-item-label>-->
+          </div>
+        </q-timeline-entry>
+      </q-timeline>
+    </q-item>
   </div>
 </template>
 
@@ -9,17 +68,11 @@ import { useQuasar } from 'quasar'
 import { computed, onMounted, ref } from 'vue'
 import { collection, onSnapshot, orderBy } from 'firebase/firestore'
 import { db } from 'src/firebase'
-import eventMonthList from 'components/Events/eventMonthList.vue'
-
-// import NewsCardDetailPopUp from 'components/NewsCardDetailPopUp'
-// const newsCardCollectionRef = collection(db, 'events', 'polpred')
-// const newsCardCollectionQuery = query(newsCardCollectionRef, orderBy('date', 'desc'))
-
-// import NewsCardDetailPopUp from 'components/NewsCardDetailPopUp'
+import NewsCardDetailPopUp from 'components/NewsCard/NewsCardDetailPopUp.vue'
 
 export default {
   name: 'eventSZFO23',
-  components: { eventMonthList },
+  components: { NewsCardDetailPopUp },
   data () {
     return {
       btnColor: 'blue-grey-10',
@@ -38,7 +91,29 @@ export default {
     const NewsCardsAu = ref([])
     const NewsCardsSep = ref([])
     const NewsCardsA = ref([])
+    const monthList = ref([])
     onMounted(async () => {
+      onSnapshot(collection(db, '/events/szfo/2023/'), orderBy('date', 'desc'), (querySnapshot) => {
+        const fbEvents = []
+        querySnapshot.forEach((doc) => {
+          const listDateEvent = {
+            id: doc.id,
+            title: doc.data().title,
+            color: doc.data().color,
+            date: doc.data().date,
+            mounth: doc.data().mounth,
+            tour: doc.data().tour,
+            eventName: doc.data().eventName,
+            time: doc.data().time,
+            icon: doc.data().icon,
+            body: doc.data().body,
+            scorer: doc.data().scorer
+          }
+          fbEvents.push(listDateEvent)
+        })
+        monthList.value = fbEvents
+        console.log(monthList)
+      })
       // NewsCard Module
       onSnapshot(collection(db, '/events/szfo/2023/may/match'), orderBy('date', 'desc'), (querySnapshot) => {
         const fbEvents = []
@@ -174,6 +249,7 @@ export default {
       titleEvent1: 'TitileEventValue1',
       btnSize: 'xs',
       NewsCards,
+      monthList,
       NewsCardsJ,
       NewsCardsJl,
       NewsCardsA,
