5789b227cc5e7a326ddd60b5ea408fc5eec223d9
lowdog136 <lowdog136@gmail.com>
2024-03-08 14:45:44 +0200

    add UI tabFootDate.vue

---
+++ src/components/NewsCard/UI/tabFootDate.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-tab :name=tabName :icon=tabIcon>{{ datenews }}
+  </q-tab>
+</template>
+
+<script>
+export default {
+  name: 'tabFootDate',
+  data () {
+    return {
+      tabIcon: 'event',
+      tabName: 'event'
+    }
+  },
+  props: {
+    datenews: String
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
