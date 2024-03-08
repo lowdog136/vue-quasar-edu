59c8b5e024785d7ab3038beb881f0e77442ce015
lowdog136 <lowdog136@gmail.com>
2024-03-08 14:17:32 +0200

    add UI tabFootStar.vue

---
+++ src/components/NewsCard/UI/tabFootStar.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-tab name="star" :icon=tabIcon :label=tabName>
+    <q-badge :color=bdgeColor  :text-color=bdgeTextColor floating>{{ tabFootStarRating }}</q-badge>
+  </q-tab>
+</template>
+
+<script>
+export default {
+  name: 'tabFootStar',
+  data () {
+    return {
+      bdgeColor: 'dark',
+      bdgeTextColor: 'white'
+    }
+  },
+  props: {
+    tabFootStarRating: String,
+    tabIcon: String,
+    tabName: String,
+    btnColor: String
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
