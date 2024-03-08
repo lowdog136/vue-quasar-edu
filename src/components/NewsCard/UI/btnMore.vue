35ce2597b6d000762ea528595d1b9a810d5dda46
lowdog136 <lowdog136@gmail.com>
2024-03-08 12:14:50 +0200

    add UI comp - btnMore.vue

---
+++ src/components/NewsCard/UI/btnMore.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-btn :label='btnName' :color=btnColor :size=btnSize />
+</template>
+
+<script>
+export default {
+  name: 'btnMore',
+  data () {
+    return {}
+  },
+  props: {
+    btnSize: String,
+    btnName: String,
+    btnColor: String
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
