a4dafe2ab449eadf83191f4091006d666edd43d0
lowdog136 <lowdog136@gmail.com>
2024-03-08 19:17:45 +0200

    add UI btnChoose.vue

---
+++ src/components/ArchiveGames/UI/btnChoose.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-btn :label=btnLabel :type=btnType :color=btnColor />
+</template>
+
+<script>
+export default {
+  name: 'btnChoose',
+  data () {
+    return {
+      btnType: 'submit',
+      btnLabel: 'Выбрать',
+      btnColor: 'primary'
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
