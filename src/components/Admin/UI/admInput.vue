8015e5188cf80fbb980e48b267f01bd4f4ec24c8
lowdog136 <lowdog136@gmail.com>
2024-03-09 13:16:26 +0200

    add new UI components

---
+++ src/components/Admin/UI/admInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-input
+    :hint=inputHint
+    :type="inputType"
+    lazy-rules
+  />
+</template>
+
+<script>
+export default {
+  name: 'admInput',
+  data () {
+    return {
+      btnColor: '#3498db'
+    }
+  },
+  props: {
+    inputModel: String,
+    inputType: String,
+    inputHint: String
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
---
+++ src/components/Admin/UI/btnAdd.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-btn :label='btnName' :color=btnColor :size=btnSize />
+</template>
+
+<script>
+export default {
+  name: 'btnAdd',
+  data () {
+    return {
+      btnName: 'add event',
+      btnColor: '#3498db'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  button
+    background-color: #3498db
+    padding: 10px 20px
+    margin-top: 10px
+    border: none
+    color: white
+</style>
