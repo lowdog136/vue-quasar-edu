5163e5137236bc8d003ae3d6bdece8269d98544f
lowdog136 <lowdog136@gmail.com>
2024-03-09 13:41:38 +0200

    add UI components

---
+++ src/components/Admin/UI/tabFootDelete.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-tab :name=tabName :icon=tabIcon />
+</template>
+
+<script>
+export default {
+  name: 'tabFootDelete',
+  data () {
+    return {
+      tabName: 'delete',
+      tabIcon: 'delete'
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
---
+++ src/components/Admin/UI/tabFootDone.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-tab :name=tabName :icon=tabIcon />
+</template>
+
+<script>
+export default {
+  name: 'tabFootDone',
+  data () {
+    return {
+      tabName: 'done',
+      tabIcon: 'done'
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
