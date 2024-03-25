9e18a324d05860bfff96e32f7756f3d1e5344693
lowdog136 <lowdog136@gmail.com>
2024-03-25 20:48:34 +0200

    commit error fix

--- src/components/Admin/Panels/sitePanel/functionsSitePanel/gamesNowEventEdit.vue
+++ src/components/Admin/Panels/sitePanel/functionsSitePanel/gamesNowEventEdit.vue
@@ -313,25 +313,25 @@ export default {
 <style lang="sass" scoped>
 .my-card
   width: 100%
-  root
-  width: 100%
-    margin: 0 auto
-    background-color: #fff
-    padding: 30px
-    margin-top: 100px
-    border-radius: 20px
-  input
-    border: none
-    outline: none
-    border-bottom: 1px solid #ddd
-    font-size: 1em
-    padding: 5px 0
-    margin: 10px 0 5px 0
-    width: 100%
-  button
-    background-color: #3498db
-    padding: 10px 20px
-    margin-top: 10px
-    border: none
-    color: white
+//.root
+//  width: 100%
+//    margin: 0 auto
+//    background-color: #fff
+//    padding: 30px
+//    margin-top: 100px
+//    border-radius: 20px
+//  input
+//    border: none
+//    outline: none
+//    border-bottom: 1px solid #ddd
+//    font-size: 1em
+//    padding: 5px 0
+//    margin: 10px 0 5px 0
+//    width: 100%
+//  button
+//    background-color: #3498db
+//    padding: 10px 20px
+//    margin-top: 10px
+//    border: none
+//    color: white
 </style>
