39504c1102e249ceac28aeb97488e6193cc083df
lowdog136 <lowdog136@gmail.com>
2024-03-25 20:50:17 +0200

    commit error fix

--- src/components/Admin/Panels/sitePanel/functionsSitePanel/GamesNowEventAdd.vue
+++ src/components/Admin/Panels/sitePanel/functionsSitePanel/GamesNowEventAdd.vue
@@ -226,25 +226,25 @@ export default {
 .my-card
   width: 100%
   max-width: 450px
-  .root
-    width: 400px
-    margin: 0 auto
-    background-color: #fff
-    padding: 30px
-    margin-top: 100px
-    border-radius: 20px
-  input
-    border: none
-    outline: none
-    border-bottom: 1px solid #ddd
-    font-size: 1em
-    padding: 5px 0
-    margin: 10px 0 5px 0
-    width: 100%
-  button
-    background-color: #3498db
-    padding: 10px 20px
-    margin-top: 10px
-    border: none
-    color: white
+  //.root
+  //  width: 400px
+  //  margin: 0 auto
+  //  background-color: #fff
+  //  padding: 30px
+  //  margin-top: 100px
+  //  border-radius: 20px
+  //input
+  //  border: none
+  //  outline: none
+  //  border-bottom: 1px solid #ddd
+  //  font-size: 1em
+  //  padding: 5px 0
+  //  margin: 10px 0 5px 0
+  //  width: 100%
+  //button
+  //  background-color: #3498db
+  //  padding: 10px 20px
+  //  margin-top: 10px
+  //  border: none
+  //  color: white
 </style>
