d3948e106f77a737c1a1d56842b8db6dd7917cd1
lowdog136 <lowdog136@gmail.com>
2024-06-12 17:35:19 +0200

    newsDrawer refactor block Club: done

---
+++ src/components/NewsDrawer/Club/expansionItems/expansionItemHistoryClub.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-item clickable v-ripple :active="active" :to=qItemTo>
+    <q-item-section avatar>
+      <q-icon :name=iconName />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label>{{ qItemLabel }}</q-item-label>
+      <q-item-label caption>{{ qItemLabelCaption }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'expansionItemHistoryClub',
+  setup () {
+    const active = ref([])
+    return {
+      active
+    }
+  },
+  props: {
+    iconName: String,
+    qItemLabel: String,
+    qItemLabelCaption: String,
+    qItemTo: String
+  }
+}
+</script>
+<style scoped lang="sass">
+</style>
---
+++ src/components/NewsDrawer/Club/expansionItems/expansionItemNewsClub.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-item clickable v-ripple :active="active" :to=qItemTo>
+    <q-item-section avatar>
+      <q-icon :name=iconName />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label>{{ qItemLabel }}</q-item-label>
+      <q-item-label caption>{{ qItemLabelCaption }}</q-item-label>
+    </q-item-section>
+    <q-item-section side top>
+      <!--            Блок "Новости клуба". Дата новости -->
+      <q-item-label v-for="NewsCard in NewsCards.slice(0,1)"
+                    :key="NewsCard.id" caption>
+        <span class="q-px-sm bg-deep-orange text-white text-italic rounded-borders">{{ NewsCard.datenews }}</span>
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+<script>
+import { onMounted, ref } from 'vue'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { db } from 'src/firebase'
+// NewsCard block
+const newsCardCollectionRef = collection(db, 'siteNews')
+const newsCardCollectionQuery = query(newsCardCollectionRef, orderBy('date', 'desc'))
+
+export default {
+  name: 'expansionItemNewsClub',
+  components: { },
+  setup () {
+    const events = ref([])
+    const NewsCards = ref([])
+    const GamesNowEvents = ref([])
+    const GamesNowTables = ref([])
+    onMounted(async () => {
+      onSnapshot(newsCardCollectionQuery, (querySnapshot) => {
+        const fbNewsCards = []
+        querySnapshot.forEach((doc) => {
+          const NewsCard = {
+            datenews: doc.data().datenews
+          }
+          fbNewsCards.push(NewsCard)
+        })
+        NewsCards.value = fbNewsCards
+      })
+    })
+    return {
+      labelAboutSite: 'О сайте',
+      NewsCards,
+      GamesNowEvents,
+      GamesNowTables,
+      events
+    }
+  },
+  props: {
+    iconName: String,
+    qItemLabel: String,
+    qItemLabelCaption: String,
+    qItemTo: String
+  }
+}
+</script>
+<style scoped lang="sass">
+
+</style>
