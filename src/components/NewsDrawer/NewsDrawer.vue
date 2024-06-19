378fa5af8804c654a9186d329c444fcf9af9f55c
lowdog136 <lowdog136@gmail.com>
2024-06-19 20:25:30 +0200

    newsDrawer refactor block Archive: done

--- src/components/NewsDrawer/NewsDrawer.vue
+++ src/components/NewsDrawer/NewsDrawer.vue
@@ -4,40 +4,7 @@
     <news-drawer-club />
     <news-drawer-event />
 <!--    Текущие соревнования-->
-    <q-expansion-item
-      v-model="expanded"
-      icon="perm_identity"
-      label="Архив"
-      caption="Фото, видео, результаты матчей"
-    >
-      <q-item clickable v-ripple :active="active" to="/Foto">
-        <q-item-section avatar>
-          <q-icon name="camera_alt" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ $store.state.NavigationListMenu[6].Name }}</q-item-label>
-          <q-item-label caption>{{ $store.state.NavigationListMenu[6].Caption }}</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item clickable v-ripple disable :active="active" to="/Video">
-        <q-item-section avatar>
-          <q-icon name="movie" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ $store.state.NavigationListMenu[7].Name }}</q-item-label>
-          <q-item-label caption>{{ $store.state.NavigationListMenu[7].Caption }}</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item clickable v-ripple :active="active" to="/ArchiveGamesBefore">
-        <q-item-section avatar>
-          <q-icon name="find_in_page" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ $store.state.NavigationListMenu[16].Name }}</q-item-label>
-          <q-item-label caption>{{ $store.state.NavigationListMenu[16].Caption }}</q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-expansion-item>
+    <news-drawer-archive />
     <news-drawer-fanat />
     <news-drawer-contact />
     <news-drawer-site />
@@ -51,10 +18,11 @@ import NewsDrawerHome from 'components/NewsDrawer/Home/newsDrawerHome.vue'
 import NewsDrawerFanat from 'components/NewsDrawer/Fanats/newsDrawerFanat.vue'
 import NewsDrawerContact from 'components/NewsDrawer/Contacts/newsDrawerContact.vue'
 import NewsDrawerSite from 'components/NewsDrawer/Site/newsDrawerSite.vue'
+import NewsDrawerArchive from 'components/NewsDrawer/Archives/newsDrawerArchive.vue'
 
 export default {
   name: 'NewsDrawer',
-  components: { NewsDrawerSite, NewsDrawerContact, NewsDrawerFanat, NewsDrawerHome, NewsDrawerEvent, NewsDrawerClub },
+  components: { NewsDrawerArchive, NewsDrawerSite, NewsDrawerContact, NewsDrawerFanat, NewsDrawerHome, NewsDrawerEvent, NewsDrawerClub },
   setup () {
     return {
       labelAboutSite: 'О сайте'
