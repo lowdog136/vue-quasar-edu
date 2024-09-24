25b81432f9d4dadafd1faa0cc1815640d63359ad
lowdog136 <lowdog136@gmail.com>
2024-09-24 21:12:46 +0200

    add new components

---
+++ src/components/Events/eventOffSeason.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="q-pa-sm wrap rounded-borders">
+    <q-item>
+      <q-timeline color="secondary">
+        <q-timeline-entry heading>
+          <div class="title" style="color: #ae0000">
+            {{ title }}
+          </div>
+        </q-timeline-entry>
+      </q-timeline>
+    </q-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'eventOffSeason',
+  components: {},
+  data () {
+    return {
+      title: 'Сезон 2024 окончен'
+    }
+  },
+  setup () {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.title
+  font-size: 28px
+  text-align: center
+  color: #2c3e50
+.my-card
+  width: 100%
+  max-width: 350px
+</style>
